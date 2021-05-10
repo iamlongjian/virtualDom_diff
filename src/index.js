b39b206025a23ae0c899ff5ccc8d9afc05b4fdf2
longer790164074 <790164074@qq.com>
2021-05-10 23:19:19 +0800

    patch

--- src/index.js
+++ src/index.js
@@ -16,9 +16,10 @@ const vNode1 = h('h1',{},'你好')
 const container = document.getElementById('container')
 patch(container,vNode1)
 
-const vNode2 = h('section',{},[
-    h('h1',{},'hello'),
-    h('h1',{},'diff')
+const vNode2 = h('h1',{},[
+    h('span',{},'1'),
+    h('span',{},'2'),
+    h('span',{},'3')
 ])
 const btn = document.getElementById('btn')
 btn.onclick = function(){
--- src/mysnabbdom/patch.js
+++ src/mysnabbdom/patch.js
@@ -10,6 +10,31 @@ export default (oldVnode,newVnode)=>{
   // 判断oldVnode,newVnode是不是同一个节点
   if(oldVnode.key === newVnode.key && oldVnode.sel === newVnode.sel){
       console.log('同一个节点')
+      // 判断是不是内存中同一个地址
+      if(oldVnode !== newVnode){
+        // 判断newVnode有没有text属性
+        if(newVnode.text){
+          // 判断新老vNode的text是否一致
+          if(newVnode.text !== oldVnode.text){
+            // 则用新的text替换旧的text
+            oldVnode.elm.innerText = newVnode.text
+          }
+        }else{
+          // 意味着newVnode有children属性
+          // 判断oldVnode有没有children属性
+          if(Array.isArray(oldVnode.children) && oldVnode.children.length > 0){
+            // 最复杂的情况
+          }else{
+            // 意味着oldVnode有text
+            // ① 清空text
+            oldVnode.elm.innerText = ''
+            // ② 将newVnode 的 children挂载到旧节点上
+            newVnode.children.forEach(t => {
+              oldVnode.elm.appendChild(createElement(t))
+            })
+          }
+        }
+      }
   }else{
      let newVnodeElm = createElement(newVnode)
       // 挂载到DOM树
